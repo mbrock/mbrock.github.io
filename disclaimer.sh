ebc2af5fe43077477f56b67dc74376f8152ba4d1
Mikael Brockman <mikael@brockman.se>
2015-11-06 19:43:51 +0000

    more disclaimers

--- disclaimer.sh
+++ disclaimer.sh
@@ -3,16 +3,20 @@ cat <<.
 <!doctype html>
 <meta charset=utf-8>
 <style>
-body { white-space: pre-wrap; font: 24px georgia, times, serif; margin: 1em; }
-em { font-variant: small-caps; text-transform: lowercase; }
+body { font: 24px georgia, times, serif; margin: 1em; max-width: 40em; }
+em { font-style: normal; }
+x { text-transform: lowercase; font-variant: small-caps; }
 </style>
 .
-curl 'http://hn.algolia.com/api/v1/search_by_date?query=disclaimer&hitsPerPage=20000' |
-jq '.hits[] | ._highlightResult.comment_text.value ' |
-grep -oE "[[(]?<em>.*</em>: [iI]([ ',-]\([^)]+\)|([ ',-]+(\w)+))+[.)\\]]*" |
+(for i in 0 1 2 3 4; do
+     curl "http://hn.algolia.com/api/v1/search_by_date?query=\"disclaimer:%20i\"&hitsPerPage=200&page=$i"	| jq -r '.hits[] | ._highlightResult.comment_text.value '
+ done) |
+grep -oE "[[(]?<em>[^<]+</em>([ ',-]\([^)]+\)|([ ',-]+(\w)+))+[.)\\]]*" |
 sed -r -e "s/^[[(]//" -e "s/(\)|\])$//" |
+sed -e "s/<em>.*disclaimer/<em>disclaimer/i" |
 sort | uniq -i |
-while read line; do echo -e "$line\n"; done
+sed -e "s,disclaimer,<x>disclaimer</x>,i" |
+while read line; do echo -e "<p>$line</p>"; done
 cat <<.
 <em>disclaimer:</em> I made this list with a <a href=disclaimer.sh>beautiful shell script</a>.
 .
